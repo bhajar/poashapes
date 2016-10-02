37f0be944870474c143bfe044ede305c1792a4af
bhajar <hajarboulhdir@gmail.com>
2016-10-02 23:18:13 +0200

    Add Translatable3D class and its test.

--- src/Translatable3D.java
+++ src/Translatable3D.java
@@ -1,6 +1,6 @@
 /**
  * Created by hajarboulhdir on 02/10/2016.
  */
-public interface Translatable3D implements Translatable2D {
+public interface Translatable3D extends Translatable2D {
     public abstract Point3D getRefPoint();
 }
---
+++ tests/Translatable2DTest.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by hajarboulhdir on 02/10/2016.
+ */
+public class Translatable2DTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRefPoint() throws Exception {
+
+    }
+
+}
