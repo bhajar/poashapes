47119350fe928efdf447ac57fdfddfcda6cbabd4
bhajar <hajarboulhdir@gmail.com>
2016-10-03 01:37:04 +0200

    Add Sphere class.

--- src/Sphere.java
+++ src/Sphere.java
@@ -3,9 +3,18 @@
  */
 public class Sphere extends Shape3D{
     private double radius;
+
+    public Sphere() {
+        this(0.0);
+    }
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+
     public double volume(){
         return( 4.0 / 3.0 ) * Math.PI * Math.pow( this.radius, 3.0 );
-
     }
 
     @Override
@@ -16,5 +25,9 @@ public class Sphere extends Shape3D{
     @Override
     public Point3D getRefPoint() {
         return null;
+    }
+
+    public double getRadius() {
+        return radius;
     }
 }
