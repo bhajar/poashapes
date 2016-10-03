e6fdc3e435cf9b0b210cd857d613c3025e7c4fcc
bhajar <hajarboulhdir@gmail.com>
2016-10-03 02:18:22 +0200

    Add  Shape2Dtest, the test related to the class Shape2D.

---
+++ tests/Shape2DTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by hajarboulhdir on 03/10/2016.
+ */
+public class Shape2DTest {
+    @Test
+    public void testPerimeter() throws Exception {
+
+    }
+
+}
